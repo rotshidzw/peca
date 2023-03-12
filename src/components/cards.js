46a285ef1dbaa7eaa2ee6638a992bd40e64167ec
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2023-03-12 22:38:32 +0200

    uploading my web page

--- src/components/cards.js
+++ src/components/cards.js
@@ -8,16 +8,21 @@ const NewsCards = () => {
   useEffect(() => {
     const fetchArticles = async () => {
       const response = await axios.get(
-        'http://api.mediastack.com/v1/news',
+        'https://api.newscatcherapi.com/v2/search',
         {
           params: {
-            access_key: 'e083fe5795d9dd5c31655ee9e5dbf6b6',
-            keywords: 'keyword',
-            limit: 2
+            q: 'keyword',
+            lang: 'en',
+            sort_by: 'relevancy',
+            page: 1,
+            page_size: 2
+          },
+          headers: {
+            'x-api-key': 'ifz5ahm7KX9sUBSQPAWNHa05L4KhXLHBmiVMZsA9ONI'
           }
         }
       );
-      setArticles(response.data.data);
+      setArticles(response.data.articles);
     };
     fetchArticles();
   }, []);
@@ -30,15 +35,15 @@ const NewsCards = () => {
     <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8 mt-20">
       {articles.map((article) => (
         <div
-          key={article.url}
+          key={article.link}
           className="bg-white rounded-lg overflow-hidden shadow-lg cursor-pointer"
           style={{ height: '480px' }}
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
-          onClick={() => handleArticleClick(article.url)}
+          onClick={() => handleArticleClick(article.link)}
         >
           <motion.img
-            src={article.image}
+            src={article.media}
             alt={article.title}
             className="w-full h-2/3 object-cover"
             style={{ height: '320px' }}
@@ -48,7 +53,7 @@ const NewsCards = () => {
             <h2 className="font-bold text-xl mb-2">{article.title}</h2>
             <div className="flex items-center mt-4">
               <div className="text-sm">
-                <p className="text-gray-900 leading-none font-bold hover:text-slate-900">{article.author} {new Date(article.published_at).toLocaleDateString()}</p>
+                <p className="text-gray-900 leading-none font-bold hover:text-slate-900">{article.author} {new Date(article.published_date).toLocaleDateString()}</p>
               </div>
             </div>
           </div>
