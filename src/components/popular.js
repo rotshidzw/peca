9b2038288fe4840e46b45447c60a307c371aeebb
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2023-03-12 23:09:58 +0200

    uploading my web page

--- src/components/popular.js
+++ src/components/popular.js
@@ -3,87 +3,88 @@ import axios from 'axios';
 import { motion } from 'framer-motion';
 
 const TrendingCards = () => {
-  const [articles, setArticles] = useState([]);
+    const [articles, setArticles] = useState([]);
 
-  useEffect(() => {
-    const fetchArticles = async () => {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=us&apiKey=798e568c8f0844d480d811eab603dbba'
-      );
-      setArticles(response.data.articles.slice(0, 6));
+    useEffect(() => {
+      const fetchArticles = async () => {
+        const response = await axios.get(
+          'https://gnews.io/api/v4/top-headlines?country=us&token=59c423d844791b0318277eddcc6ee045'
+        );
+        setArticles(response.data.articles.slice(0, 6));
+      };
+      fetchArticles();
+    }, []);
+  
+    const handleArticleClick = (url) => {
+      window.open(url, '_blank');
     };
-    fetchArticles();
-  }, []);
 
-  const handleArticleClick = (url) => {
-    window.open(url, '_blank');
-  };
-
-  return (
-    <div className="container mx-auto mt-14">
-      <h1 className="text-4xl text-left font-bold mb-8">Most Popular</h1>
-      <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
-      <div className="md:col-span-3">
-  <div className="grid grid-cols-1 gap-6">
-    {articles.slice(0, 3).map((article) => (
-      <div
-      key={article.url}
-      className="bg-white rounded-lg overflow-hidden shadow-lg cursor-pointer flex flex-row"
-      whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.95 }}
-      onClick={() => handleArticleClick(article.url)}
-    >
-      <div className="w-1/2">
-        <motion.img
-          src={article.urlToImage}
-          alt={article.title}
-          className="w-full h-72 object-cover"
-          whileHover={{ scale: 1.1 }}
-        />
-      </div>
-      <div className="w-1/2 p-6">
-        <div className="flex items-start justify-start">
-          <div className="bg-blue-500 text-white font-bold text-sm px-2 py-1 rounded-md">
-            {article.source.name}
+    return (
+        <div className="container mx-auto mt-14">
+          <h1 className="text-4xl text-left font-bold mb-8">Most Popular</h1>
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+          <div className="md:col-span-3">
+      <div className="grid grid-cols-1 gap-6">
+        {articles.slice(0, 3).map((article) => (
+          <div
+          key={article.url}
+          className="bg-white rounded-lg overflow-hidden shadow-lg cursor-pointer flex flex-row"
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          onClick={() => handleArticleClick(article.url)}
+        >
+          <div className="w-1/2">
+            <motion.img
+              src={article.image}
+              alt={article.title}
+              className="w-full h-72 object-cover"
+              whileHover={{ scale: 1.1 }}
+            />
           </div>
-          <p className="text-gray-600 ml-2 text-sm">{new Date(article.publishedAt).toLocaleDateString()}</p>
+          <div className="w-1/2 p-6">
+            <div className="flex items-start justify-start">
+              <div className="bg-blue-500 text-white font-bold text-sm px-2 py-1 rounded-md">
+                {article.source.name}
+              </div>
+              <p className="text-gray-600 ml-2 text-sm">{new Date(article.publishedAt).toLocaleDateString()}</p>
+            </div>
+            <h2 className="font-bold text-xl mb-2 mt-16">{article.title}</h2>
+            <div className="flex items-center mt-8">
+              <p className="text-gray-900 leading-none font-bold hover:text-slate-900 mr-2">{article.author}</p>
+              <a
+                href={article.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-sm text-gray-600 hover:text-gray-900"
+              >
+                Read more
+              </a>
+            </div>
+          </div>
         </div>
-        <h2 className="font-bold text-xl mb-2 mt-16">{article.title}</h2>
-        <div className="flex items-center mt-8">
-          <p className="text-gray-900 leading-none font-bold hover:text-slate-900 mr-2">{article.author}</p>
-          <a
-            href={article.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm text-gray-600 hover:text-gray-900"
-          >
-            Read more
-          </a>
-        </div>
+        
+        ))}
       </div>
     </div>
-    
-    ))}
-  </div>
-</div>
         <div className="md:col-span-2">
-        <div className="grid grid-cols-1 gap-6">
-  {articles.slice(3, 5).map((article) => (
-    <div
-      key={article.url}
-      className="bg-white rounded-lg overflow-hidden shadow-lg cursor-pointer"
-      whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.95 }}
-      onClick={() => handleArticleClick(article.url)}
-    >
-      <div className="flex items-start justify-start">
-        <div className="bg-blue-500 text-white font-bold text-sm px-2 py-1 rounded-md">
-          {article.source.name}
+          <div className="grid grid-cols-1 gap-6">
+            {articles.slice(3, 5).map((article) => (
+              <div
+                key={article.link}
+                className="bg-white rounded-lg overflow-hidden shadow-lg cursor-pointer"
+                whileHover={{ scale: 1.05 }}
+                whileTap={{ scale: 0.95 }}
+                onClick={() => handleArticleClick(article.link)}
+              >
+                <div className="flex items-start justify-start">
+                  <div className="bg-blue-500 text-white
+                     font-bold text-sm px-2 py-1 rounded-md">
+                {article.source.name}
         </div>
         <p className="text-gray-600 ml-2 text-sm">{new Date(article.publishedAt).toLocaleDateString()}</p>
       </div>
       <motion.img
-        src={article.urlToImage}
+        src={article.image}
         alt={article.title}
         className="w-full h-48 object-cover"
         whileHover={{ scale: 1.1 }}
